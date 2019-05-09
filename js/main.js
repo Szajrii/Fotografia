a65b884ce953fe06eb1823f4a3f87941e2d244d5
Szajrii <michal05v@gmail.com>
2019-05-09 21:12:48 +0200

    All section almost finished
    Zmiana koncepcji strony, przyjrzeć się jak wygląda jeszcze na mobilnych, szczególnie na tabletach, czcionki powiększyć.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,16 @@
 new Vue({
 	el: "#app",
 	data: {
-		leftSquare:{
-			left: 0,
-			top: 100
-		},
-		rightSquare:{
-			left: 0,
-			top: 0
-		},
 		offsetTop: 0,
-		aboutOffset: document.getElementById('about').offsetTop,
-		joinOffset: document.getElementById('join').offsetTop,
-		totalOffset: this.aboutOffset - this.offsetTop,
-		lastScrollTop: 0,
-		scrollDirection: '',
-		resize: true
+				
 	},
 	methods:{
 		scrollListener: function(){ 
 			this.offsetTop = window.pageYOffset;
 		},
-		test: function(){
-			this.leftSquare.left = 7;
-			this.leftSquare.top =  300;
-
-			this.rightSquare.left =  -7;
-			this.rightSquare.top = 100;
-		},
 		getScrollDirection:function(){
 		
-		       var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+		       var st = window.pageYOffset || document.documentElement.scrollTop; 
 			   if (st > this.lastScrollTop){
 			      // downscroll code
 			      this.scrollDirection = "down"
@@ -43,45 +23,7 @@ new Vue({
 		}
 	},
 	watch:{
-		offsetTop: function(){
-			
-			// if(this.offsetTop  > this.aboutOffset - 350 || this.offsetTop  > this.joinOffset - 150){
-			// 	this.resize = false
-			// }
-			if(this.offsetTop  > this.aboutOffset - 300){
-				this.leftSquare.left = 7;
-				this.leftSquare.top =  75;
-
-				this.rightSquare.left =  -7;
-				this.rightSquare.top = 25;
-			}
-
-			if(this.offsetTop  > this.joinOffset - 300 || this.offsetTop  < this.aboutOffset ){
-				this.leftSquare.left = 0;
-				this.leftSquare.top =  100;
-
-				this.rightSquare.left =  0;
-				this.rightSquare.top = 0;
-			}
-
-			// if(this.offsetTop  > this.aboutOffset - 400){
-			// 	if(this.offsetTop  < this.joinOffset - 200){
-			// 		if(this.leftSquare.left < 7 && this.scrollDirection == "down" && this.resize == true){
-			// 			this.leftSquare.left = 7;
-			// 			this.leftSquare.top =  300;
-
-			// 			this.rightSquare.left =  -7;
-			// 			this.rightSquare.top = 100;
-			// 		}else if(this.leftSquare.left >= 7 && this.scrollDirection == "up" && this.resize == false){
-			// 			this.leftSquare.left = 0;
-			// 			this.leftSquare.top =  400;
-
-			// 			this.rightSquare.left =  0;
-			// 			this.rightSquare.top = 0;
-			// 		}
-			// 	}
-			// }
-		}
+		
 	},
 	created(){
 		window.addEventListener('scroll', this.scrollListener);
